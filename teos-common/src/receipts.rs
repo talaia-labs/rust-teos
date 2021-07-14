deff5705684e422d99877b470b002246217cae61
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2021-07-14 17:08:48 +0200

    Merges RegisterReceipt implementations and adds AppointmentReceipt

--- teos-common/src/receipts.rs
+++ teos-common/src/receipts.rs
@@ -11,6 +11,15 @@ pub struct RegistrationReceipt {
 }
 
 impl RegistrationReceipt {
+    pub fn new(user_id: UserId, available_slots: u32, subscription_expiry: u32) -> Self {
+        RegistrationReceipt {
+            user_id,
+            available_slots,
+            subscription_expiry,
+            signature: None,
+        }
+    }
+
     pub fn user_id(&self) -> &UserId {
         &self.user_id
     }
@@ -26,23 +35,53 @@ impl RegistrationReceipt {
     pub fn signature(&self) -> Option<String> {
         self.signature.clone()
     }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        let mut ser = Vec::new();
+        ser.extend_from_slice(&self.user_id.0.serialize());
+        ser.extend_from_slice(&self.available_slots.to_be_bytes());
+        ser.extend_from_slice(&self.subscription_expiry.to_be_bytes());
+
+        ser
+    }
+
+    pub fn sign(&mut self, sk: SecretKey) {
+        // TODO: Check if there's any case where this can actually fail. Don't unwrap if so.
+        self.signature = Some(cryptography::sign(&self.serialize(), sk).unwrap());
+    }
+}
+#[derive(Debug)]
+pub struct AppointmentReceipt {
+    user_signature: String,
+    start_block: u32,
+    signature: Option<String>,
 }
 
-impl RegistrationReceipt {
-    pub fn new(user_id: UserId, available_slots: u32, subscription_expiry: u32) -> Self {
-        RegistrationReceipt {
-            user_id,
-            available_slots,
-            subscription_expiry,
+impl AppointmentReceipt {
+    pub fn new(user_signature: String, start_block: u32) -> Self {
+        AppointmentReceipt {
+            user_signature,
+            start_block,
             signature: None,
         }
     }
 
+    pub fn user_signature(&self) -> String {
+        self.user_signature.clone()
+    }
+
+    pub fn start_block(&self) -> u32 {
+        self.start_block
+    }
+
+    pub fn signature(&self) -> Option<String> {
+        self.signature.clone()
+    }
+
     pub fn serialize(&self) -> Vec<u8> {
         let mut ser = Vec::new();
-        ser.extend_from_slice(&self.user_id.serialize());
-        ser.extend_from_slice(&self.available_slots.to_be_bytes());
-        ser.extend_from_slice(&self.subscription_expiry.to_be_bytes());
+        ser.extend_from_slice(&self.user_signature.as_bytes());
+        ser.extend_from_slice(&self.start_block.to_be_bytes());
 
         ser
     }
