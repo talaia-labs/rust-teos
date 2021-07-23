51a89dac6977dbeb41edf58fdaba87e00f560350
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2021-07-23 12:43:05 +0200

    Updates ChainMonitor to receive a ChainPoller instead of a BitcoindClient
    The BitcoindClient was being used only to build a Poller, so passing the latter to the constructor simplifies putting all the components together.
    
    WIP: For now, the ChainPoller is passed as is, instead of as a Rc<RefCell<T>> or Arch<Mutex<T>>.

--- teos/src/chain_monitor.rs
+++ teos/src/chain_monitor.rs
@@ -1,49 +1,52 @@
-use std::ops::Deref;
-use std::sync::Arc;
+use std::ops::DerefMut;
 use std::{thread, time};
 use tokio::sync::broadcast::Sender;
 
-use bitcoin::network::constants::Network;
-use lightning_block_sync::init::validate_best_block_header;
 use lightning_block_sync::poll::{ChainPoller, ChainTip, Poll, ValidatedBlockHeader};
+use lightning_block_sync::BlockSource;
 use lightning_block_sync::BlockSourceError;
 
-use crate::bitcoin_cli::BitcoindClient;
-
-pub struct ChainMonitor {
-    bitcoin_cli: Arc<BitcoindClient>,
+pub struct ChainMonitor<B: DerefMut<Target = T> + Sized, T: BlockSource> {
+    poller: ChainPoller<B, T>,
+    last_known_block_header: ValidatedBlockHeader,
     polling_delta: time::Duration,
     tx: Sender<ValidatedBlockHeader>,
 }
 
-impl ChainMonitor {
+impl<B, T> ChainMonitor<B, T>
+where
+    B: DerefMut<Target = T> + Sized + Send + Sync,
+    T: BlockSource,
+{
     pub async fn new(
-        bitcoin_cli: Arc<BitcoindClient>,
+        poller: ChainPoller<B, T>,
+        last_known_block_header: ValidatedBlockHeader,
         polling_delta_sec: u64,
         tx: Sender<ValidatedBlockHeader>,
     ) -> Self {
         ChainMonitor {
-            bitcoin_cli,
+            poller,
+            last_known_block_header,
             polling_delta: time::Duration::from_secs(polling_delta_sec),
             tx,
         }
     }
 
-    pub async fn monitor_chain(self) -> Result<(), BlockSourceError> {
-        let mut derefed = self.bitcoin_cli.deref();
-        let mut current_best = validate_best_block_header(&mut derefed).await.unwrap();
-        let mut poller = ChainPoller::new(&mut derefed, Network::Bitcoin);
-
+    pub async fn monitor_chain(&mut self) -> Result<(), BlockSourceError> {
         loop {
-            match poller.poll_chain_tip(current_best).await {
+            match self
+                .poller
+                .poll_chain_tip(self.last_known_block_header)
+                .await
+            {
                 Ok(chain_tip) => match chain_tip {
                     ChainTip::Common => {
                         println!("No new tip found");
                     }
                     ChainTip::Better(new_best) => {
-                        println!("New tip found: {:?}", new_best);
-                        current_best = new_best;
-                        self.tx.send(current_best).unwrap();
+                        println!("New tip found: {}", new_best.header.block_hash());
+                        self.last_known_block_header = new_best;
+                        self.tx.send(new_best).unwrap();
                     }
 
                     ChainTip::Worse(worse) => {
@@ -54,9 +57,9 @@ impl ChainMonitor {
                         // never happen under reasonable assumptions (e.g. cache of 6 blocks).
                         println!("Worse tip found: {:?}", worse);
 
-                        if worse.chainwork == current_best.chainwork {
-                            current_best = worse;
-                            self.tx.send(current_best).unwrap();
+                        if worse.chainwork == self.last_known_block_header.chainwork {
+                            self.last_known_block_header = worse;
+                            self.tx.send(worse).unwrap();
                         } else {
                             println!("New tip has less work than the previous one")
                         }
