eacb2446f95fa8ccbef973513c394b5e09d355b2
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2021-12-13 11:31:15 +0100

    Adds missing teos-common docs. Updates teos/lib main description

--- teos-common/src/constants.rs
+++ teos-common/src/constants.rs
@@ -1,6 +1,12 @@
+//! Shared constant values.
+
 // LN general nomenclature
+/// Number of blocks required to consider a transaction irrevocable.
 pub const IRREVOCABLY_RESOLVED: u32 = 100;
 
 // Temporary constants, may be changed
+/// Maximum size of encrypted blobs in appointments.
 pub const ENCRYPTED_BLOB_MAX_SIZE: usize = 2048;
+/// Size of the outdated users cache.
+// FIXME: this may not fit here.
 pub const OUTDATED_USERS_CACHE_SIZE_BLOCKS: usize = 10;
