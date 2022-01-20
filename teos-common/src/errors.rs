af396055f2723beeee9dba6c062e8dbd329c3701
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2022-01-20 15:07:39 +0100

    Removes unnecesary api errors

--- teos-common/src/errors.rs
+++ teos-common/src/errors.rs
@@ -10,8 +10,7 @@ pub const INVALID_SIGNATURE_OR_SUBSCRIPTION_ERROR: u8 = 7;
 /// Appointment errors [33, 64]
 pub const APPOINTMENT_FIELD_TOO_SMALL: u8 = 33;
 pub const APPOINTMENT_FIELD_TOO_BIG: u8 = 34;
-pub const APPOINTMENT_WRONG_FIELD: u8 = 35;
-pub const APPOINTMENT_ALREADY_TRIGGERED: u8 = 36;
+pub const APPOINTMENT_ALREADY_TRIGGERED: u8 = 35;
 
 /// Registration errors [65, 96]
 pub const REGISTRATION_RESOURCE_EXHAUSTED: u8 = 65;
