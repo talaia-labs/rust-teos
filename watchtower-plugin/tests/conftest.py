f6decde5ff364cccfdc7b5ba7c9e20a38fadd30d
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2024-06-08 12:16:34 +0200

    modernize cln tests and CI

--- watchtower-plugin/tests/conftest.py
+++ watchtower-plugin/tests/conftest.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import subprocess
 
 from pyln.testing.fixtures import *  # noqa: F401,F403
-from pyln.testing.utils import DEVELOPER, BITCOIND_CONFIG, TailableProc
+from pyln.testing.utils import BITCOIND_CONFIG, TailableProc
 
 WT_PLUGIN = Path("~/.cargo/bin/watchtower-client").expanduser()
 TEOSD_CONFIG = {
@@ -114,19 +114,6 @@ def pytest_runtest_makereport(item, call):
     # be "setup", "call", "teardown"
 
     setattr(item, "rep_" + rep.when, rep)
-
-
-def pytest_configure(config):
-    config.addinivalue_line("markers", "developer: only run when developer is flagged on")
-
-
-def pytest_runtest_setup(item):
-    for mark in item.iter_markers(name="developer"):
-        if not DEVELOPER:
-            if len(mark.args):
-                pytest.skip("!DEVELOPER: {}".format(mark.args[0]))
-            else:
-                pytest.skip("!DEVELOPER: Requires DEVELOPER=1")
 
 
 @pytest.fixture(scope="function", autouse=True)
